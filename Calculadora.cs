fe3f2c033de739d9f05d70b02885ce4a0bf0cdb9
ianbe <ianbernardolim@gmail.com>
2024-05-14 07:25:47 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Talents
+{
+    public class Calculadora
+    {
+        public int somar(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            return 0;
+        }
+        public int dividir(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public List<string> historico() 
+        {
+            return new List<string>();
+        }
+    }
+}
