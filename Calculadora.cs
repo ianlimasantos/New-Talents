361d66402f2b9f8a7889126012b015e000ebe7da
ianbe <ianbernardolim@gmail.com>
2024-05-14 20:05:38 -0300

    Finalização da calculadora

--- Calculadora.cs
+++ Calculadora.cs
@@ -8,28 +8,45 @@ namespace New_Talents
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+        private string data;
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
         public int somar(int val1, int val2)
         {
-            return 0;
+            int res = val1 + val2;
+            listaHistorico.Insert(0, "Resultado: " + res + " - data: " + data);
+            return res;
         }
 
         public int subtrair(int val1, int val2)
         {
-            return 0;
+            int res = val1 - val2;
+            listaHistorico.Insert(0, "Resultado: " + res + " - data: " + data);
+            return res;
         }
 
         public int multiplicar(int val1, int val2)
         {
-            return 0;
+            int res = val1 * val2;
+            listaHistorico.Insert(0, "Resultado: " + res + " - data: " + data);
+            return res;
         }
         public int dividir(int val1, int val2)
         {
-            return 0;
+            int res = val1 / val2;
+            listaHistorico.Insert(0, "Resultado: " + res + " - data: " + data);
+            return res;
         }
 
         public List<string> historico() 
         {
-            return new List<string>();
+            List<string> res;
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
         }
     }
 }
